84495d28ba4d5a5972e1403533c32264bca6e555
hslee <vawav11@gmail.com>
2024-05-16 20:59:18 +0900

    init commit

---
+++ gpblog/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/NFAP0221S.github.io",
+  assetPrefix: "/NFAP0221S.github.io/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
