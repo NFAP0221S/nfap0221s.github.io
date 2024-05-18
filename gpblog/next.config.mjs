6a6cf6fe85073a5befac706d928a911c016e9b15
hslee <vawav11@gmail.com>
2024-05-18 22:10:25 +0900

    dev환경 설정

--- gpblog/next.config.mjs
+++ gpblog/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
-  basePath: "/NFAP0221S.github.io",
-  assetPrefix: "/NFAP0221S.github.io/",
+  basePath: isProd ? "/NFAP0221S.github.io" : "",
+  assetPrefix: isProd ? "/NFAP0221S.github.io/" : "",
   images: {
     unoptimized: true,
   },
