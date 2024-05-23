6217c10d380c189027b23dadd445b0243f63ae1c
hslee <vawav11@gmail.com>
2024-05-23 23:30:21 +0900

    code, image test

--- gpblog/src/app/posts/[id]/page.tsx
+++ gpblog/src/app/posts/[id]/page.tsx
@@ -17,9 +17,9 @@ export default async function Post({ params }: any) {
     notFound();
   }
 
-  const  isChildren = async (id: string) => {
+  const  isChildren = async (id: string, text: string) => {
     const blocks = await getBlocks(id);
-    console.log('블록3', blocks[0]?.bulleted_list_item)
+    // console.log(text, blocks[0]?.bulleted_list_item)
   } 
 
   const renderRichText = (richTextArray: any[]) => {
@@ -31,6 +31,7 @@ export default async function Post({ params }: any) {
   };
 
   const renderBlock = (block: any) => {
+    console.log('zzzzs',block)
     switch (block.type) {
       case 'paragraph':
         return <p>{renderRichText(block.paragraph.rich_text)}</p>;
@@ -41,9 +42,8 @@ export default async function Post({ params }: any) {
       case 'heading_3':
         return <h3>{renderRichText(block.heading_3.rich_text)}</h3>;
       case 'bulleted_list_item':
-        console.log('블록', block)
         if(block.has_children){
-          isChildren(block.id)
+          isChildren(block.id, '점칠드런')
         }
         return <li>{renderRichText(block.bulleted_list_item.rich_text)}</li>;
       case 'numbered_list_item':
@@ -59,13 +59,42 @@ export default async function Post({ params }: any) {
         );
       case 'toggle':
         // console.log('토글', block.toggle)
+        if(block.has_children){
+          isChildren(block.id, '토글칠드런')
+        }
         return (
           <details>
             <summary>{renderRichText(block.toggle.rich_text)}</summary>
             <div>{block.children && block.children.map(renderBlock)}</div>
           </details>
         );
+      case 'code':
+        // console.log('토글', block.toggle)
+        return (
+          <pre>
+          <code>
+            {renderRichText(block.code.rich_text)}
+          </code>
+        </pre>
+        );
+      case 'image':
+        console.log('ㄴㄴㄴㄴ', block.image.caption[0].plain_text)
+        return (
+          <figure>
+            <img src={block.image.file.url} alt={block.image.caption} style={{ maxWidth: '100%' }} />
+            {block.image.caption && <figcaption>{block.image.caption[0].plain_text}</figcaption>}
+          </figure>
+        );
+      // default:
+      //   console.log('디폴트', block.image)
+      //   return (
+      //     <figure>
+      //       <img src={block.image.url} alt={block.image.caption} style={{ maxWidth: '100%' }} />
+      //       {block.image.caption && <figcaption>{block.image.caption}</figcaption>}
+      //     </figure>
+      //   );
       default:
+        console.log('디폴트', block)
         return <div>Unsupported block type: {block.type}</div>;
     }
   };
