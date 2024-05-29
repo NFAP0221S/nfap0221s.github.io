fea8dcf185564b51018eda9e10a2d03af2260e90
hslee <vawav11@gmail.com>
2024-05-29 22:17:47 +0900

    loading 삭제

--- gpblog/src/app/(blog)/_components/posts/PostsList.tsx
+++ gpblog/src/app/(blog)/_components/posts/PostsList.tsx
@@ -1,27 +1,12 @@
 'use client';
 import { Post } from "@/lib/notion";
-
 import PostLink from "./PostLink";
-import { useEffect, useState } from "react";
-import Loading from "@/app/loading";
 
 interface Props{
   list: Post[]
 }
 
 const PostsList: React.FC<Props> = ({ list }) => {
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // 강제 로드를 위해 2초 후에 로딩 상태를 false로 변경
-    const timer = setTimeout(() => setLoading(false), 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
 
   return (
     <ul>
