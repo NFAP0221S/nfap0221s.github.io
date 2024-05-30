d113d56c338afa8f65df6ed4d5abd375f6da5dad
hslee <vawav11@gmail.com>
2024-05-30 23:15:26 +0900

    autoprefixer

--- gpblog/src/app/(blog)/_components/layout/Sidebar.tsx
+++ gpblog/src/app/(blog)/_components/layout/Sidebar.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
 const Sidebar = () => (
-  <aside className="fixed top-0 left-0 z-40 w-48 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div>사이드 바</div>
-    <section>
-      <div>Frontend</div>
+  <aside className="fixed top-0 left-0 z-40 w-48 h-screen bg-gray-100 shadow-lg transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <div className="p-4">사이드 바</div>
+    <section className="p-4">
+      <div className="font-bold">Frontend</div>
       <ul>
         <li><a href="/posts">- Post 1</a></li>
         <li><a href="/posts">- Post 2</a></li>
       </ul>
     </section>
-    <section>
-      <div>Backend</div>
+    <section className="p-4">
+      <div className="font-bold">Backend</div>
       <ul>
         <li><a href="/posts">- Post 1</a></li>
         <li><a href="/posts">- Post 2</a></li>
       </ul>
     </section>
-    <section>
-      <div>Devops</div>
+    <section className="p-4">
+      <div className="font-bold">DevOps</div>
       <ul>
         <li><a href="/posts">- Post 1</a></li>
         <li><a href="/posts">- Post 2</a></li>
