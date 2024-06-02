62ceb59543a15a1ae860567e0baa0f1cce4b8739
hslee <vawav11@gmail.com>
2024-06-02 15:56:31 +0900

    .

--- gpblog/next.config.mjs
+++ gpblog/next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 
+const prefix =
+  process.env.NODE_ENV === 'production' ? 'https://nfap0221s.github.io' : ""
+
+
 const nextConfig = {
   output: "export",
   // basePath: "",
-  basePath: isProd ? "/nfap0221s.github.io" : "",
+  // basePath: isProd ? "/nfap0221s.github.io" : "",
+  // assetPrefix: isProd ? "/nfap0221s.github.io/" : "",
+  basePath: prefix,
   assetPrefix: isProd ? "/nfap0221s.github.io/" : "",
   swcMinify: true,
   trailingSlash: true,
