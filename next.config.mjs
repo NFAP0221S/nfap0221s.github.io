c55ddf63344dfca03935b61a3136c16c102b00b9
hslee <vawav11@gmail.com>
2024-06-05 20:20:09 +0900

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,15 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: "export",
+  reactStrictMode: true,
   basePath: "",
-  // assetPrefix: "",
-  // basePath: isProd ? "/nfap0221s.github.io" : "",
   assetPrefix: isProd ? "/" : "",
-  // swcMinify: true,
-  // trailingSlash: true,
+  swcMinify: true,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  
 };
 
 export default nextConfig;
