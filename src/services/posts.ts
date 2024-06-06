9c40db4d16fab19c1b5e6d37f741b610cfbfdf4a
hslee <vawav11@gmail.com>
2024-06-06 22:20:41 +0900

    Update posts.ts

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,6 +1,6 @@
 import { getDatabase } from "@/lib/notion";
 
-export default async function fetchPosts() {
+export async function fetchPosts() {
   const databaseId = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID as string;
   return await getDatabase(databaseId);
 };
