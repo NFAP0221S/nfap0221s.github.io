c2c19b219b9860fe255f1773447e30f191ac908f
hslee <vawav11@gmail.com>
2024-06-13 22:51:48 +0900

    test

--- src/app/(blog)/_components/Header.tsx
+++ src/app/(blog)/_components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 
   return(
     // <header className={`flex items-center justify-around p-4`}>
-    <header className="fixed top-0 left-0 w-full flex items-center justify-around p-4 z-50 bg-opacity-80 backdrop-blur-sm ">
+    <header className="sticky top-0 w-full flex items-center justify-around p-4 z-50 bg-opacity-80 backdrop-blur-sm">
       <nav className='flex items-center'>
         <Link href="/" className="mr-4">Home</Link>
         <Link href="/about" className="mr-4">About</Link>
