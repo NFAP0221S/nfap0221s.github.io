5eaacd2e8fc9faf2d8ff3d2b5818ebb360e2feae
hslee <vawav11@gmail.com>
2024-06-19 23:44:09 +0900

    Update page.tsx

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -4,10 +4,10 @@ import { usePosts } from "@/app/hooks";
 export default function Home() {
   const { data: categoryList, isLoading, error } = usePosts();
   
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading posts: {error.message}</div>;
-  console.log('categoryList', categoryList)
-  console.log('isLoading', isLoading)
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>Error loading posts: {error.message}</div>;
+  // console.log('categoryList', categoryList)
+  // console.log('isLoading', isLoading)
   return (
     <div className={`p-4`}>
       <h1>Home 입니다.</h1>
